76c5aa4d14a12abbda64c00f93603506c1511aea
Tom de Grunt <tom@degrunt.nl>
2025-04-13 21:21:21 +0200

    Fixes

--- src/lazyvim/install.sh
+++ src/lazyvim/install.sh
@@ -3,8 +3,5 @@ set -e
 
 echo "Activating feature 'lazyvim'"
 
-VERSION=${VERSION:-stable}
-echo "The version to be installed is: $VERSION"
-
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
